3629913403055e64311f246f2233daf4a560a8d4
Sean S. LeBlanc <sean.s.leblanc@gmail.com>
2021-11-13 16:06:37 -0500

    fix: make text style type `Partial`
    all of the text style properties are optional based on the implementation, but the current type definition requires them all to be defined

--- index.d.ts
+++ index.d.ts
@@ -2,11 +2,11 @@ import { Sprite } from '@pixi/sprite';
 import { TextStyle } from '@pixi/text';
 
 export class HTMLText extends Sprite {
-    constructor(text?:string, style?:TextStyle, canvas?:HTMLCanvasElement);
+    constructor(text?:string, style?:Partial<TextStyle>, canvas?:HTMLCanvasElement);
     readonly canvas:HTMLCanvasElement;
     readonly context:CanvasRenderingContext2D;
     text:string;
-    style:TextStyle;
+    style:Partial<TextStyle>;
     resolution: number;
     updateText(respectDirty?:boolean): void;
 }
