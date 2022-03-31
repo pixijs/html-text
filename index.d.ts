228d6c51b10a0954ead57fa1284d1bac8cd22eec
ChasLui <chaslui@outlook.com>
2022-03-31 12:39:46 +0800

    Added support of css styles

--- index.d.ts
+++ index.d.ts
@@ -2,11 +2,12 @@ import { Sprite } from '@pixi/sprite';
 import { TextStyle } from '@pixi/text';
 
 export class HTMLText extends Sprite {
-    constructor(text?:string, style?:TextStyle, canvas?:HTMLCanvasElement);
+    constructor(text?:string, style?:PIXI.TextStyle, canvas?:HTMLCanvasElement, cssStyle?: Record<string, string>);
     readonly canvas:HTMLCanvasElement;
     readonly context:CanvasRenderingContext2D;
     text:string;
     style:TextStyle;
+    cssStyle:Record<string, string>;
     resolution: number;
     updateText(respectDirty?:boolean): void;
 }
