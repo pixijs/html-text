1c8bdbedb9fbdda4b0a4908087f2e665089c9062
ChasLui <chaslui@outlook.com>
2022-04-01 10:16:10 +0800

    fix: cssStyle type defined

--- index.d.ts
+++ index.d.ts
@@ -2,7 +2,7 @@ import { Sprite } from '@pixi/sprite';
 import { TextStyle } from '@pixi/text';
 
 export class HTMLText extends Sprite {
-    constructor(text?:string, style?:PIXI.TextStyle, canvas?:HTMLCanvasElement, cssStyle?: Record<string, string>);
+    constructor(text?:string, style?:PIXI.TextStyle, canvas?:HTMLCanvasElement, cssStyle?: Record<string, Record<string, boolean>>);
     readonly canvas:HTMLCanvasElement;
     readonly context:CanvasRenderingContext2D;
     text:string;
