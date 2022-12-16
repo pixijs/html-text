68c6dab5fd9088590ab76b84446bcb0627b256a1
Matt Karl <matt@mattkarl.com>
2022-12-16 09:04:47 -0500

    Workaround for Safari font loading bug (#32)

--- src/HTMLText.ts
+++ src/HTMLText.ts
@@ -186,7 +186,7 @@ export class HTMLText extends Sprite
      * @param {boolean} respectDirty - Whether to abort updating the
      *        text if the Text isn't dirty and the function is called.
      */
-    updateText(respectDirty = true): void
+    async updateText(respectDirty = true): Promise<void>
     {
         const { style, canvas, context } = this;
 
@@ -211,24 +211,30 @@ export class HTMLText extends Sprite
 
         if (!this._loading)
         {
-            const image = this._image;
-            const svgURL = new XMLSerializer().serializeToString(this._svgRoot);
-
             this._loading = true;
-            image.src = `data:image/svg+xml;charset=utf8,${encodeURIComponent(svgURL)}`;
-            image.onload = () =>
+            await new Promise<void>((resolve) =>
             {
-                context.clearRect(0, 0, canvas.width, canvas.height);
-                context.drawImage(
-                    image,
-                    0, 0, width, height,
-                    0, 0, width, height,
-                );
-                image.src = '';
-                image.onload = null;
-                this._loading = false;
-                this.updateTexture();
-            };
+                const image = this._image;
+
+                image.onload = async () =>
+                {
+                    await style.onBeforeDraw();
+                    context.clearRect(0, 0, canvas.width, canvas.height);
+                    context.drawImage(
+                        image,
+                        0, 0, width, height,
+                        0, 0, width, height,
+                    );
+                    image.src = '';
+                    image.onload = null;
+                    this._loading = false;
+                    this.updateTexture();
+                    resolve();
+                };
+                const svgURL = new XMLSerializer().serializeToString(this._svgRoot);
+
+                image.src = `data:image/svg+xml;charset=utf8,${encodeURIComponent(svgURL)}`;
+            });
         }
     }
 
