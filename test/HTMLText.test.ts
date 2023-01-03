65f7c29f64aab9c138f3db8a653fb4876a7b03ab
Matt Karl <matt@mattkarl.com>
2023-01-03 15:04:01 -0500

    Fix: Remove the usage of ShadowRoot (#36)

--- test/HTMLText.test.ts
+++ test/HTMLText.test.ts
@@ -13,38 +13,6 @@ describe('HTMLText', () =>
         text.destroy();
     });
 
-    it('should clean up the shadow element', () =>
-    {
-        const query = '[data-pixi-html-text]';
-
-        expect(document.querySelector(query)).toBeFalsy();
-
-        const text = new HTMLText('Hello world!');
-
-        expect(document.querySelector(query)).toBeTruthy();
-
-        text.destroy();
-
-        expect(document.querySelector(query)).toBeFalsy();
-    });
-
-    it('should clean up the shadow element multiples', () =>
-    {
-        const query = '[data-pixi-html-text]';
-
-        expect(document.querySelector(query)).toBeFalsy();
-
-        const text = new HTMLText('Hello world!');
-        const text2 = new HTMLText('Hello world2!');
-
-        expect(document.querySelector(query)).toBeTruthy();
-
-        text.destroy();
-        text2.destroy();
-
-        expect(document.querySelector(query)).toBeFalsy();
-    });
-
     describe('measureText', () =>
     {
         it('should measure default text', () =>
